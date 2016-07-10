9137223e6b669106cc8917ffa355f7cbc57b3d23
4shadoww <4shadoww0@gmail.com>
2016-07-10 10:15:20 +0300

    update

--- core/log.py
+++ core/log.py
@@ -6,14 +6,14 @@ logfilename = str(time)
 logfile = open('core/log/'+logfilename+'.log', 'a')
 
 def printlog(*log):
-	global time
+	time = datetime.datetime.now()
 	global logfile
 	line = str(time)+' '+str(log[0])+'\n\n'
 	logfile.write(line)
 	print(time,str(log[0])+'\n')
 
 def log(*log):
-	global time
+	time = datetime.datetime.now()
 	global logfile
 	line = str(time)+' '+str(log[0])+'\n\n'
 	logfile.write(line)
