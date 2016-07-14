f9de4674c296e14f5fd6d8ed06c7accfc38cbee5
4shadoww <4shadoww0@gmail.com>
2016-07-14 11:45:59 +0300

    fixed bug

--- core/fixreflist.py
+++ core/fixreflist.py
@@ -9,35 +9,39 @@ def fixreflist(article, text):
 	checktext = text
 	saves = ''
 	zeroedit = 0
-	if '</ref>' in text and '{{viitteet}}' not in text and '<references/> ' not in text:
+	if '</ref>' in text and '{{viitteet}}' not in text and '<references/> ' not in text and '{{Viitteet}}' not in text:
 		if '==Viitteet==' not in text and '==Lähteet==' not in text and '== Viitteet ==' not in text and '== Lähteet ==' not in text:
 			textlen = len(text)
 			usenextline = 0
 			placingposition = 0
 			for line in reversed(text.split('\n')):
 				if '{{Tynkä' in line:
-					print('tynkä')
 					usenextline = 1
 				elif '[[Luokka:' in line:
-					print('luokka')
 					usenextline = 1
+				elif line == '':
+					continue
 				elif usenextline == 1 and '*' in line[0:1]:
-					print('*')
+					placingposition = text.rfind(line)+len(line)
+					break
+				elif usenextline == 1 and '{{kesken}}' in line:
+					placingposition = text.rfind(line)+len(line)
+					break
+				elif usenextline == 1 and '{{juonipaljastus loppu}}' in line:
+					placingposition = text.rfind(line)+len(line)
+					break
+				elif usenextline == 1 and '{{Kesken}}' in line:
 					placingposition = text.rfind(line)+len(line)
 					break
 				elif usenextline == 1 and '{{' in line[0:2]:
-					print('{{')
 					continue
 				elif usenextline == 1 and line == '':
-					print('linebreak')
-					continue 
+					continue
 				elif usenextline == 1:
-					print('useline')
 					placingposition = text.rfind(line)+len(line)
 					break
 
 				else:
-					print('else')
 					placingposition = text.rfind(line)+len(line)
 					break
 			if placingposition != 0:
@@ -47,22 +51,60 @@ def fixreflist(article, text):
 				errorcout += 1
 				error = 1
 		else:
+			addreferences = 0
+			endposition = 0
+			foundlist = 0
 			for line in text.split('\n'):
 				if '==Viitteet==' in line or '==Lähteet==' in line or '== Viitteet ==' in line or '== Lähteet ==' in line:
 					startposition = text.rfind(line)
 					if line == '==Viitteet==':
 						endposition = startposition + 12
-					elif line == '==Lähteet==':
-						endposition = startposition + 11
 					elif line == '== Viitteet ==':
 						endposition = startposition + 14
+					elif line == '===Viitteet===':
+						endposition = startposition + 14
+					elif line == '=== Viitteet ===':
+						endposition = startposition + 16
+					elif line == '==Lähteet==':
+						addreferences = 1
+						endposition = startposition + 11
 					elif line == '== Lähteet ==':
+						addreferences = 1
 						endposition = startposition + 13
-					text_data = list(text)
-					text_data.insert(endposition, '\n{{viitteet}}')
-					text = ''.join(text_data)
-					errorcout += 1
-					error = 2
+					if endposition != 0:
+						if '*' in text[endposition:endposition+3]:
+							lastposition = 0
+							lastlen = 0
+							for line in text[endposition:].split('\n'):
+								if '*' in line:
+									foundlist = 1
+									lastposition = text[endposition:].find(line)
+									lastlen = len(line)
+								if '*' not in line and lastlen != 0 and lastposition != 0:
+									if addreferences == 1 and foundlist == 1:
+										endposition = endposition + lastposition + lastlen
+										text_data = list(text)
+										text_data.insert(endposition, '\n\n===Viitteet===\n{{viitteet}}\n')
+										text = ''.join(text_data)
+										errorcout += 1
+										error = 1
+										break
+
+									else:
+										endposition = endposition + lastposition + lastlen
+										text_data = list(text)
+										text_data.insert(endposition, '\n{{viitteet}}')
+										text = ''.join(text_data)
+										errorcout += 1
+										error = 2
+										break
+
+						else:
+							text_data = list(text)
+							text_data.insert(endposition, '\n{{viitteet}}')
+							text = ''.join(text_data)
+							errorcout += 1
+							error = 2
 
 
 	if text != oldtext:
