658de93b37229333f3483aac453507f4e118dcc2
4shadoww <4shadoww0@gmail.com>
2016-07-18 20:08:12 +0300

    updated

--- core/smallfix.py
+++ core/smallfix.py
@@ -3,7 +3,7 @@ import re
 from core.config import *
 
 def smallfix(article, text):
-	errorcout = 0
+	errorcount = 0
 	text = str(text)
 	oldtext = text
 	checktext = text
@@ -16,26 +16,25 @@ def smallfix(article, text):
 		if 'small' in item and len(item) <= 10:
 			if '/' in item and item != '</small>':
 				text = text.replace(item, '</small>')
-				errorcout += 1
+				errorcount += 1
 			elif '/' not in item and item != '<small>':
 				text = text.replace(item, '<small>')
-				errorcout += 1
+				errorcount += 1
 
 	
 	if text != oldtext:
 		zeroedit = 1
 		printlog('smallfix error found: '+ article)
-		if errorcout > 1 and lang == 'fi':
+		if errorcount > 1 and lang == 'fi':
 			saves = u"Botti korjasi small tagien syntaksit. "
-		elif errorcout == 1 and lang == 'fi':
+		elif errorcount == 1 and lang == 'fi':
 			saves = u"Botti korjasi small tagin syntaksin. "
-		elif errorcout > 1 and lang == 'en':
+		elif errorcount > 1 and lang == 'en':
 			saves = u"Bot has fixed small tags syntaxes. "
-		elif errorcout == 1 and lang == 'en':
+		elif errorcount == 1 and lang == 'en':
 			saves = u"Bot has fixed small tag syntax. "
 
-	elif errorcout == 0:
+	elif errorcount == 0:
 		printlog('smallfix error found: '+ article)
-		oldtext = text
 
-	return errorcout, text, saves, zeroedit
+	return errorcount, text, saves, zeroedit
