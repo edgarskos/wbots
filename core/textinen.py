4fbc838843c95d02a75e750fedcd3864f0ddf697
4shadoww <4shadoww0@gmail.com>
2016-07-18 20:08:29 +0300

    updated

--- core/textinen.py
+++ core/textinen.py
@@ -3,30 +3,29 @@ import re
 from core.config import *
 
 def textinen(article, text):
-	errorcout = 0
+	errorcount = 0
 	text = str(text)
 	oldtext = text
 	checktext = text
 	saves = ''
 	zeroedit = 0
 	
-	errorcout += text.count('[[Category:')+text.count('[[category:')
+	errorcount += text.count('[[Category:')+text.count('[[category:')
 
 	text = text.replace('[[Category:', '[[Luokka:').replace('[[category:', '[[Luokka:')
 
 	if text != oldtext:
 		printlog('textinen error found: '+ article)
-		if errorcout > 1 and lang == 'fi':
+		if errorcount > 1 and lang == 'fi':
 			saves = u"Botti muutti englanninkielisen luokka viitteen suomenkieliseksi. "
-		elif errorcout == 1 and lang == 'fi':
+		elif errorcount == 1 and lang == 'fi':
 			saves = u"Botti muutti englanninkieliset luokka viitteet suomenkielisiksi. "
-		elif errorcout > 1 and lang == 'en':
+		elif errorcount > 1 and lang == 'en':
 			saves = u""
-		elif errorcout == 1 and lang == 'en':
+		elif errorcount == 1 and lang == 'en':
 			saves = u""
 
-	elif errorcout == 0:
+	elif errorcount == 0:
 		printlog('textinen error not found: '+ article)
-		oldtext = text
 
-	return errorcout, text, saves, zeroedit
+	return errorcount, text, saves, zeroedit
