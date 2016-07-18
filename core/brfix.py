a8e9010bbfcb1ce90428972d9f10830916d1ad96
4shadoww <4shadoww0@gmail.com>
2016-07-18 20:06:01 +0300

    updated

--- core/brfix.py
+++ core/brfix.py
@@ -3,7 +3,7 @@ import re
 from core.config import *
 
 def brfix(article, text):
-	errorcout = 0
+	errorcount = 0
 	text = str(text)
 	oldtext = text
 	checktext = text
@@ -17,34 +17,33 @@ def brfix(article, text):
 			if 'clear' in item and '=' in item:
 				if 'all' in item:
 					text = text.replace(item, '{{clear}}')
-					errorcout += 1
+					errorcount += 1
 				elif 'left' in item:
 					text = text.replace(item, '{{clear|left}}')
-					errorcout += 1
+					errorcount += 1
 				elif 'right' in item:
 					text = text.replace(item, '{{clear|right}}')
-					errorcout += 1
+					errorcount += 1
 			elif '/' in item and item != '<br />' and 'clear' not in item and '=' not in item:
 				text = text.replace(item, '<br />')
-				errorcout += 1
+				errorcount += 1
 			elif '/' not in item and item != '<br>' and 'clear' not in item and '=' not in item:
 				text = text.replace(item, '<br>')
-				errorcout += 1
+				errorcount += 1
 
 	if text != oldtext:
 		zeroedit = 1
 		printlog('brfix error found: '+ article)
-		if errorcout > 1 and lang == 'fi':
+		if errorcount > 1 and lang == 'fi':
 			saves = u"Botti korjasi br tagien syntaksit tai korvasi ne {{clear}} mallinnella. "
-		elif errorcout == 1 and lang == 'fi':
+		elif errorcount == 1 and lang == 'fi':
 			saves = u"Botti korjasi br tagin syntaksin tai korvasi sen {{clear}} mallinnella. "
-		elif errorcout > 1 and lang == 'en':
+		elif errorcount > 1 and lang == 'en':
 			saves = u"Bot has fixed br tags syntaxes or did replace it with {{clear}}. "
-		elif errorcout == 1 and lang == 'en':
+		elif errorcount == 1 and lang == 'en':
 			saves = u"Bot has fixed br tag syntax or did replace it with {{clear}}. "
 
-	elif errorcout == 0:
+	elif errorcount == 0:
 		printlog('brfix error found: '+ article)
-		oldtext = text
 
-	return errorcout, text, saves, zeroedit
+	return errorcount, text, saves, zeroedit
