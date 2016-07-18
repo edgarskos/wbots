037f1d3e06341a2d38238de99314d8c78bd8a6cc
4shadoww <4shadoww0@gmail.com>
2016-07-18 20:07:07 +0300

    updated

--- core/fixpiped.py
+++ core/fixpiped.py
@@ -5,7 +5,7 @@ from core.log import *
 
 
 def fixpiped(article ,text):
-	errorcout = 0
+	errorcount = 0
 	saves = ''
 	zeroedit = 0
 	text = str(text)
@@ -24,23 +24,22 @@ def fixpiped(article ,text):
 				fixeditem = '['+str(item[0])+''
 				fixeditem = fixeditem.replace('_', ' ')
 			if fixeditem != None:
-				errorcout += 1
+				errorcount += 1
 				originalitem = originalitem.replace('_', ' ')
 				printlog('fixpiped invalid links found: '+article+'\n'+originalitem+'] --> '+fixeditem+']')
 				text = text.replace(str(originalitem), str(fixeditem))
 	if text != oldtext:
 		printlog('fixpiped error found: '+ article)
-		if errorcout > 1 and lang == 'fi':
+		if errorcount > 1 and lang == 'fi':
 			saves = u'Botti poisti wikipedian sisäisistä linkeistä tekstin joissa se on sama kuin linkki. '
-		elif errorcout == 1 and lang == 'fi':
+		elif errorcount == 1 and lang == 'fi':
 			saves = u'Botti poisti wikipedian sisäisestä linkistä tekstin jossa se on sama kuin linkki. '
-		elif errorcout > 1 and lang == 'en':
+		elif errorcount > 1 and lang == 'en':
 			saves = u"Bot has removed texts from pipedlinks because it's same as link. "
-		elif errorcout == 1 and lang == 'en':
+		elif errorcount == 1 and lang == 'en':
 			saves = u"Bot has removed text from pipedlink because it's same as link. "
 
-	elif errorcout == 0:
+	elif errorcount == 0:
 		printlog('fixpiped error not found: '+ article)
-		oldtext = text
 
-	return errorcout, text, saves, zeroedit
+	return errorcount, text, saves, zeroedit
