d08591010ccf9cf7e8e553b74a8d79b58f70cc08
4shadoww <4shadoww0@gmail.com>
2016-07-18 20:05:49 +0300

    updated

--- core/adiffer.py
+++ core/adiffer.py
@@ -8,16 +8,19 @@ def high_light(seqm):
 	green= '\033[32m'
 	red = '\033[91m'
 	end = '\033[0m'
+	bold = '\033[1m'
+	underline = '\033[4m'
+	bu = bold+underline
 	output= []
 	for opcode, a0, a1, b0, b1 in seqm.get_opcodes():
 		if opcode == 'equal':
 			output.append(seqm.a[a0:a1])
 		elif opcode == 'insert':
-			output.append(green + seqm.b[b0:b1] + end)
+			output.append(bu + green + seqm.b[b0:b1] + end)
 		elif opcode == 'delete':
-			output.append(red + seqm.a[a0:a1] + end)
+			output.append(bu + red + seqm.a[a0:a1] + end)
 		elif opcode == 'replace':
-			print("what to do with 'replace' opcode?")
+			output.append(bu + green + seqm.b[b0:b1] + end)
 		else:
 			print("unexpected opcode")
 	print(''.join(output))
