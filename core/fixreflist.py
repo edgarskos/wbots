ffae041e1d8b0ce12723309f46ae296bb3e08af9
4shadoww <4shadoww0@gmail.com>
2016-07-18 20:07:30 +0300

    updated

--- core/fixreflist.py
+++ core/fixreflist.py
@@ -3,13 +3,13 @@ import re
 from core.config import *
 
 def fixreflist(article, text):
-	errorcout = 0
+	errorcount = 0
 	text = str(text)
 	oldtext = text
 	checktext = text
 	saves = ''
 	zeroedit = 0
-	if '</ref>' in text and '{{viitteet}}' not in text and '<references/> ' not in text and '{{Viitteet}}' not in text and '<references />' not in text:
+	if '</ref>' in text and '{{viitteet' not in text and '<references/>' not in text and '{{Viitteet' not in text and '<references />' not in text:
 		if '==Viitteet==' not in text and '==Lähteet==' not in text and '== Viitteet ==' not in text and '== Lähteet ==' not in text:
 			textlen = len(text)
 			usenextline = 0
@@ -33,7 +33,7 @@ def fixreflist(article, text):
 					text_data = list(text)
 					text_data.insert(position, '\n==Lähteet==\n{{viitteet}}\n\n')
 					text = ''.join(text_data)
-					errorcout += 1
+					errorcount += 1
 					error = 3
 
 			else:
@@ -55,7 +55,7 @@ def fixreflist(article, text):
 					elif usenextline == 1 and '*' in line[0:2]:
 						placingposition = text.rfind(line)+len(line)
 						break
-					elif usenextline == 1 and '{{kesken}}' in line:
+					elif usenextline == 1 and '{{kesken' in line:
 						placingposition = text.rfind(line)+len(line)
 						break
 					elif usenextline == 1 and '{{juonipaljastus loppu}}' in line:
@@ -84,7 +84,7 @@ def fixreflist(article, text):
 					text_data = list(text)
 					text_data.insert(placingposition, '\n\n==Lähteet==\n{{viitteet}}\n')
 					text = ''.join(text_data)
-					errorcout += 1
+					errorcount += 1
 					error = 3
 		else:
 			addreferences = 0
@@ -115,23 +115,23 @@ def fixreflist(article, text):
 						endposition = startposition + 13
 						break
 			if endposition != 0:
-				if '*' in text[endposition:endposition+3] or '{{Commons' in text[endposition:endposition+11]:
+				if '*' in text[endposition:endposition+3] or '{{Commons' in text[endposition:endposition+11] or '{{commons' in text[endposition:endposition+11] or '{{Kirjaviite'  in text[endposition:endposition+20] or '{{kirjaviite'  in text[endposition:endposition+20] or '{{Lehtiviite'  in text[endposition:endposition+20] or '{{lehtiviite'  in text[endposition:endposition+20] or '{{Verkkoviite'  in text[endposition:endposition+20] or '{{verkkoviite'  in text[endposition:endposition+20] or '{{Karttaviite' in text[endposition:endposition+20] or '{{karttaviite' in text[endposition:endposition+20] or '{{Standardiviite' in text[endposition:endposition+20] or '{{standardiviite' in text[endposition:endposition+20]:
 					lastposition = 0
 					lastlen = 0
 					time = 0
 					for line in text[endposition:].split('\n'):
 						time += 1
-						if '*' in line or '{{Commons' in line:
+						if '*' in line or '{{Commons' in line or '{{commons' in line:
 							foundlist = 1
 							lastposition = text[endposition:].find(line)
 							lastlen = len(line)
-						if '*' not in line and '{{Commons' not in line and '|' not in line and '{{IMDb-h' not in line and lastlen != 0 and lastposition != 0 or time == len(text[endposition:].split('\n')):
+						if '*' not in line and '{{Commons' not in line and '{{commons' not in line and '{{kirjaviite' not in line and '{{Kirjaviite' not in line and '{{Lehtiviite' not in line and '{{lehtiviite' not in line and '{{Verkkoviite' not in line and '{{verkkoviite' not in line and '{{Karttaviite' not in line and '{{karttaviite' not in line and '{{Standardiviite' not in line and '{{standardiviite' not in line and '|' not in line and '{{IMDb-h' not in line and lastlen != 0 and lastposition != 0 or time == len(text[endposition:].split('\n')):
 							if addreferences == 1 and foundlist == 1:
 								endposition = endposition + lastposition + lastlen
 								text_data = list(text)
 								text_data.insert(endposition, '\n\n===Viitteet===\n{{viitteet}}\n')
 								text = ''.join(text_data)
-								errorcout += 1
+								errorcount += 1
 								error = 1
 								break
 
@@ -140,7 +140,7 @@ def fixreflist(article, text):
 								text_data = list(text)
 								text_data.insert(endposition, '\n{{viitteet}}')
 								text = ''.join(text_data)
-								errorcout += 1
+								errorcount += 1
 								error = 2
 								break
 
@@ -148,27 +148,26 @@ def fixreflist(article, text):
 					text_data = list(text)
 					text_data.insert(endposition, '\n{{viitteet}}')
 					text = ''.join(text_data)
-					errorcout += 1
+					errorcount += 1
 					error = 2
 
 
 	if text != oldtext:
 		zeroedit = 1
 		printlog('fixreflist error found: '+ article)
-		if errorcout == 1 and lang == 'fi' and error == 1:
+		if errorcount == 1 and lang == 'fi' and error == 1:
 			saves = u"Botti lisäsi puuttuvan viitteet osion. "
-		elif errorcout == 1 and lang == 'fi' and error == 2:
+		elif errorcount == 1 and lang == 'fi' and error == 2:
 			saves = u"Botti lisäsi puuttuvan viitteet mallinen. "
-		elif errorcout == 1 and lang == 'fi' and error == 3:
+		elif errorcount == 1 and lang == 'fi' and error == 3:
 			saves = u"Botti lisäsi puuttuvan lähteet osion. "
-		elif errorcout == 1 and lang == 'en' and error == 2:
+		elif errorcount == 1 and lang == 'en' and error == 2:
 			saves = u"Bot has added missing references template. "
-		elif errorcout == 1 and lang == 'en' and error == 1:
+		elif errorcount == 1 and lang == 'en' and error == 1:
 			saves = u"Bot has added missing references section. "
 
 
-	elif errorcout == 0:
+	elif errorcount == 0:
 		printlog('fixreflist error not found: '+ article)
-		oldtext = text
 
-	return errorcout, text, saves, zeroedit
+	return errorcount, text, saves, zeroedit
