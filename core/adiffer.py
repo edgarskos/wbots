430d27d3e25c34d8cfc5a5ce09e99a49034caee6
4shadoww <4shadoww0@gmail.com>
2016-07-19 10:10:54 +0300

    update

--- core/adiffer.py
+++ core/adiffer.py
@@ -24,3 +24,27 @@ def high_light(seqm):
 		else:
 			print("unexpected opcode")
 	print(''.join(output))
+
+def show_diffl(oldtext, text):
+	sm= difflib.SequenceMatcher(None, oldtext, text)
+	high_light2(sm)
+
+def high_light2(seqm):
+	green= '\033[32m'
+	red = '\033[91m'
+	end = '\033[0m'
+	whg = '\033[1;47m'
+	ehg = '\033[1;m'
+	output= []
+	for opcode, a0, a1, b0, b1 in seqm.get_opcodes():
+		if opcode == 'equal':
+			output.append(seqm.a[a0:a1])
+		elif opcode == 'insert':
+			output.append(whg + green + seqm.b[b0:b1] + end + ehg)
+		elif opcode == 'delete':
+			output.append(whg + red + seqm.a[a0:a1] + end + ehg)
+		elif opcode == 'replace':
+			output.append(whg + green + seqm.b[b0:b1] + end + ehg)
+		else:
+			print("unexpected opcode")
+	print(''.join(output))
