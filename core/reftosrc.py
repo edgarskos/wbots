2bf8033e3729480cd038cd3af872e0b63c08ff0f
4shadoww <4shadoww0@gmail.com>
2016-07-20 09:51:45 +0300

    updated

--- core/reftosrc.py
+++ core/reftosrc.py
@@ -5,21 +5,33 @@ def reftosrc(article, text):
 	errorcount = 0
 	text = str(text)
 	oldtext = text
-	checktext = text
 	saves = ''
 	zeroedit = 0
+	error = 0
 	
 	
 	textlist = text.split('\n')
-	for l,i in enumerate(textlist):
-		if i=='==Viitteet==' or i=='== Viitteet ==':
-			textlist[l]='==Lähteet=='
+	if '==Lähteet==' in text or '== Lähteet ==' in text or '== Lähteet==' in text or '==Lähteet ==' in text:
+		for l,i in enumerate(textlist):
+			if i=='==Viitteet==' or i=='== Viitteet ==' or i=='== Viitteet==' or i=='==Viitteet ==':
+				textlist[l]='===Viitteet==='
+				error = 1
+	else:
+		for l,i in enumerate(textlist):
+			if i=='==Viitteet==' or i=='== Viitteet ==' or i=='== Viitteet==' or i=='==Viitteet ==':
+				textlist[l]='==Lähteet=='
+				error = 2
 
 	text = '\n'.join(textlist)
 	if text != oldtext:
 		zeroedit = 1
+		errorcount += 1
 		printlog('reftosrc error found: '+ article)
-		saves = u"Botti muutti ==Vitteet== osion muotoon ==Lähteet==. "
+		if error == 1:
+			saves = u"Botti siirsi Viitteet osion oikealle tasolle. "
+		elif error == 2:
+			saves = u"Botti muutti ==Vitteet== osion muotoon ==Lähteet==. "
+
 
 	elif errorcount == 0:
 		printlog('reftosrc error found: '+ article)
